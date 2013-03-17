5dc8e46e5d18332a51c31b63c9622a9c272353f2
Jonathan Taylor <taylorj7@rose-hulman.edu>
2013-03-17 14:05:28 -0400

    Piece class and PieceTests implemented

--- Sorry!/src/BoardList.java
+++ Sorry!/src/BoardList.java
@@ -1,4 +1,18 @@
 
 public class BoardList {
-	
+	Node[] cornerPointers;
+	public BoardList(){
+		
+	}
+	public String toString(){
+		return toString(cornerPointers[0]);
+	}
+	public String toString(Node start){
+		Node temp = start;
+		String ret = "";
+		while(temp.next != start){
+			
+		}
+	}
 }
+
