1f72e50bdbd309e35d654f0950ce4c4f3c3b01b9
Jonathan Taylor <taylorj7@rose-hulman.edu>
2013-03-19 19:59:43 -0400

    board actually appearing now

--- Sorry!/src/DisplayableBoard.java
+++ Sorry!/src/DisplayableBoard.java
@@ -3,23 +3,27 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 public class DisplayableBoard extends JComponent {
 	private static final long serialVersionUID = 1L;
 	BoardList board;
-	private static ImageIcon image;
-	public DisplayableBoard(BoardList board){
+	private static BufferedImage image;
+
+	public DisplayableBoard(BoardList board) {
 		super();
-		this.setSize(1000, 1000);
 		this.board = board;
-//		File fil = new File("Board.jpg");
-		image = new ImageIcon("Board.jpg");
-		this.setBackground(Color.GREEN);
+		try {
+			image = ImageIO.read(this.getClass().getResource(
+					"/images/Board.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	public void paintComponent(Graphics g){
-		image.paintIcon(this,  g, 100, 100);
+
+	public void paintComponent(Graphics g) {
+		g.drawImage(image, 0, 0, null);
 	}
 }
