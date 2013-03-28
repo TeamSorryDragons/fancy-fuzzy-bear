71ad70d8acb0380370a6371783acc337577a8058
Jonathan Taylor <taylorj7@rose-hulman.edu>
2013-03-28 18:56:20 -0400

    fixed node stuff and some move stuff

---
+++ Sorry!/src/InvalidMoveException.java
@@ -0,0 +1,11 @@
+
+public class InvalidMoveException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public InvalidMoveException(String string) {
+		super(string);
+	}
+	public InvalidMoveException(){
+		super();
+	}
+
+}
