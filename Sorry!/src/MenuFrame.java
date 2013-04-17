f021c592c65e770258371ec68b54df19e8c9bb39
Dylan Sturgeon <sturgedl@rose-hulman.edu>
2013-04-17 13:15:31 -0400

    updated menu frame

--- Sorry!/src/MenuFrame.java
+++ Sorry!/src/MenuFrame.java
@@ -1,8 +1,27 @@
+import java.awt.Dimension;
+import java.awt.Font;
 
-public class MenuFrame {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-	public MenuFrame(String string) {
-		// TODO Auto-generated constructor stub
+/**
+ * JFrame to display menu options to the user. Used to select game settings then
+ * initiate a new game.
+ * 
+ * @author sturgedl. Created Apr 17, 2013.
+ */
+public class MenuFrame extends JFrame {
+	private static final int TITLE_WIDTH = 1000;
+	private static final int TITLE_HEIGHT = 100;
+
+	public MenuFrame(String lang) {
+		JPanel titleLabel = new JPanel();
+		JLabel title = new JLabel("Sorry!©");
+		title.setFont(new Font("Georgia", Font.BOLD, 60));
+		titleLabel.setPreferredSize(new Dimension(TITLE_WIDTH, TITLE_HEIGHT));
+
+		
 	}
 
 }
